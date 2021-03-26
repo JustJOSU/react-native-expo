c03ad88fa51cf31686311ed799632bfd9ae7cb4c
sj.jo <xc11161@gmail.com>
2021-03-26 19:40:10 +0900

    updated HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,30 +2,44 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Image, View, ScrollView, Text } from 'react-native';
 import { io } from 'socket.io-client';
 import axios from 'axios';
+import { FlatList } from 'react-native-gesture-handler';
 
-const socket = io('http://172.20.10.2:3000');
 
-const HomeScreen = () => {
-    const [code, setCode] = useState({});
-    axios.get('http://172.20.10.2:3000/searchMarketCode')
-        .then(data => {
-            setCode([data.data]);
-        })
+// const socket = io('http://172.20.10.2:3000');
 
-    const [text, setText] = useState('');
+function HomeScreen() {
+    const [data, setData] = useState({ codes: [] });
+    
     useEffect(() => {
-        socket.on("connect", () => {
-            socket.on("message", (data) => {
-                setText(data)
-            })
-        })
-    })
+        let ignore = false;
+        
+        async function fetchData() {
+            const result = await axios.get('http://192.168.35.164:3000/searchMarketCode');
+            const coinList = [];
+            for(let value of result.data){
+                let temp = {}
+                temp[value] = 0;
+                coinList.push(temp);
+            }
+            if(!ignore) setData(coinList);
+        }
+
+        fetchData();
+        return () => { ignore = true; }
+    }, [])
+    // useEffect(() => {
+    //     socket.on("connect", () => {
+    //         socket.on("message", (data) => {
+    //             setText(data)
+    //         })
+    //     })
+    // })
     return (
         <View style={styles.container}>
             <View style={{ flex: 1 }} />
             <View style={{ flex: 6, backgroundColor: "blue" }}>
                 <ScrollView>
-                    <Text>{console.log(code)}</Text>
+                    <Text>{console.log(data)}</Text>
                     {/* <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
                     <Image source={logo} />
                     <Image source={logo} />

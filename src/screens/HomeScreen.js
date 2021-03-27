eeabc0e7616b6e2e567a189047b28a4bea3fcb58
sj.jo <xc11161@gmail.com>
2021-03-27 13:48:41 +0900

    create request test code

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,27 +6,38 @@ import { FlatList } from 'react-native-gesture-handler';
 
 
 // const socket = io('http://172.20.10.2:3000');
-
+let ignore = false;
 function HomeScreen() {
-    const [data, setData] = useState({ codes: [] });
-    
-    useEffect(() => {
-        let ignore = false;
-        
-        async function fetchData() {
-            const result = await axios.get('http://192.168.35.164:3000/searchMarketCode');
-            const coinList = [];
-            for(let value of result.data){
-                let temp = {}
-                temp[value] = 0;
-                coinList.push(temp);
-            }
-            if(!ignore) setData(coinList);
-        }
+    const [data, setData] = useState({});
+    async function getMarket() {
+        const result = await axios.get('http://192.168.35.164:3000/searchMarketCode');
+        setData(result.data)
+    }
+    if(!ignore) {
+        getMarket();
+        ignore = true;
+    }
 
-        fetchData();
-        return () => { ignore = true; }
-    }, [])
+
+
+    
+    // useEffect(() => {
+    //     let ignore = false;
+        
+    //     async function fetchData() {
+    //         const result = await axios.get('http://192.168.35.164:3000/searchMarketCode');
+    //         const coinList = [];
+    //         for(let value of result.data){
+    //             let temp = {}
+    //             temp[value] = 0;
+    //             coinList.push(temp);
+    //         }
+    //         if(!ignore) setData(coinList);
+    //     }
+
+    //     fetchData();
+    //     return () => { ignore = true; }
+    // }, [])
     // useEffect(() => {
     //     socket.on("connect", () => {
     //         socket.on("message", (data) => {
@@ -39,7 +50,7 @@ function HomeScreen() {
             <View style={{ flex: 1 }} />
             <View style={{ flex: 6, backgroundColor: "blue" }}>
                 <ScrollView>
-                    <Text>{console.log(data)}</Text>
+                    <Text>{data.krw}</Text>
                     {/* <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
                     <Image source={logo} />
                     <Image source={logo} />
